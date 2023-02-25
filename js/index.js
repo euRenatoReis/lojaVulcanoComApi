ebfa44b29576426b0302184a4260fbe0d9647ae2
Renato Vieira <115581375+euRenatoReis@users.noreply.github.com>
2023-02-25 14:47:24 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,50 +5,72 @@ const nomeproduto = document.querySelector('.nomeproduto');
 const areaDoPreco = document.querySelector('.preco');
 const imagemproduto = document.querySelector('.fotodoproduto');
 
-const setavoltar = document.querySelector('.setavoltar');
-const setaavancar = document.querySelector('.setaavancar');
 const carrossel = document.querySelectorAll('.carrossel');
+const setaavancar = document.querySelector('.setaavancar');
+const setavoltar = document.querySelector('.setavoltar');
 
-barradebusca.addEventListener('input', async () => {
+let indiceAtual = 0;
+
+setaavancar.addEventListener('click', () => {
+  carrossel[indiceAtual].classList.remove('aberto');
+  indiceAtual = (indiceAtual + 1) % carrossel.length;
+  carrossel[indiceAtual].classList.add('aberto');
+});
+
+setavoltar.addEventListener('click', () => {
+  carrossel[indiceAtual].classList.remove('aberto');
+  indiceAtual = (indiceAtual - 1 + carrossel.length) % carrossel.length;
+  carrossel[indiceAtual].classList.add('aberto');
+});
+
+
+
+
+// api
+
+barradebusca.addEventListener('input', pegaValorDaApi())
+
+barradebusca.addEventListener('keyup', async (e)=>{
+
+  const pegarValorEvento = e.target.value; 
+  const pressionado = e.which || e.keycode;
+  const chave = pressionado === 13;
+
+  pegaValorDaApi();
+})
+
+
+async function pegaValorDaApi(){
+
   try {
     const searchQuery = barradebusca.value;
     const response = await fetch(`https://fakestoreapi.com/products?title=${searchQuery}`);
     const products = await response.json();
+
     mostrarResultados(products);
+
   } catch (error) {
     console.log('Ocorreu um erro ao buscar produtos', error);
   }
-});
+
+  return true
+}
 
 function mostrarResultados(products) {
+
   if (products.length === 0) {
     mostrador.classList.add('oculto');
     return;
   }
-  const primeiroProduto = products[0];
-  areaDoPreco.innerHTML = primeiroProduto.price;
-  nomeproduto.innerHTML = primeiroProduto.title;
+
+  const procura = products.find(function(products){
+
+      return products.value == searchQuery
+
+  });
+
+  areaDoPreco.innerHTML = procura.price;
+  nomeproduto.innerHTML = procura.title;
+
   mostrador.classList.remove('oculto');
 }
-
-
-setaavancar.addEventListener('click', () => {
-
-  const indeceAtual = 0;
-
-  for (var i = 0; i < carrossel.length; i++) {
-
-    indeceAtual++;
-    carrossel[indeceAtual];
-
-  }
-
-  
-  console.log(carrossel);
-
-})
-
-setavoltar.addEventListener('click', () => {
-
-
-})
---
+++ js/objects/obj.js
@@ -0,0 +1,14 @@
+
+const info = {
+
+   produto: '',
+
+   preco: '',
+
+   
+
+
+
+
+
+}
