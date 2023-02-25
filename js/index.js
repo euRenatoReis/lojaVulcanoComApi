fb7d391d46f2df1a607f1fdeb53ef11c20bdccf3
Renato Vieira <115581375+euRenatoReis@users.noreply.github.com>
2023-02-25 15:05:18 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -28,46 +28,42 @@ setavoltar.addEventListener('click', () => {
 
 // api
 
-barradebusca.addEventListener('input', pegaValorDaApi())
-
 barradebusca.addEventListener('keyup', async (e)=>{
-
   const pegarValorEvento = e.target.value; 
   const pressionado = e.which || e.keycode;
   const chave = pressionado === 13;
-
-  pegaValorDaApi();
+  
+  // chamar a função pegaValorDaApi sem os parênteses
+  await pegaValorDaApi();
 })
 
-
 async function pegaValorDaApi(){
-
   try {
     const searchQuery = barradebusca.value;
     const response = await fetch(`https://fakestoreapi.com/products?title=${searchQuery}`);
     const products = await response.json();
 
     mostrarResultados(products);
-
   } catch (error) {
     console.log('Ocorreu um erro ao buscar produtos', error);
   }
-
-  return true
 }
 
 function mostrarResultados(products) {
-
   if (products.length === 0) {
     mostrador.classList.add('oculto');
     return;
   }
 
+  const searchQuery = barradebusca.value;
   const procura = products.find(function(products){
-
-      return products.value == searchQuery
-
+    return products.title.toLowerCase() === searchQuery.toLowerCase();
   });
+
+  if (!procura) {
+    mostrador.classList.add('oculto');
+    return;
+  }
 
   areaDoPreco.innerHTML = procura.price;
   nomeproduto.innerHTML = procura.title;
